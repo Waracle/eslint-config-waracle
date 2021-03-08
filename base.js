57a73dc92ca0da98171d4555cbe5997e1546cf72
Nick Hammond <n.hammond@waracle.com>
2021-03-08 11:01:38 +0000

    feat(sonarcloud): Add sonarcloud config
    BREAKING CHANGE: eslint-config-prettier v8 uses different config
    
    Signed-off-by: Nick Hammond <n.hammond@waracle.com>

--- base.js
+++ base.js
@@ -1,46 +1,52 @@
 module.exports = {
-  extends: ["eslint:recommended", "airbnb", "prettier"],
+  extends: ['eslint:recommended', 'airbnb-base', 'prettier'],
+  overrides: [
+    {
+      files: ['**/*.spec.ts'],
+      env: {
+        jest: true,
+      },
+      plugins: ['jest'],
+      rules: {
+        'jest/no-disabled-tests': 'warn',
+        'jest/no-focused-tests': 'error',
+        'jest/no-identical-title': 'error',
+        'jest/prefer-to-have-length': 'warn',
+        'jest/valid-expect': 'error',
+      },
+    },
+  ],
+  plugins: ['import'],
   rules: {
-    "no-shadow": "off",
-    "import/extensions": [
-      "error",
-      "ignorePackages",
+    'arrow-body-style': [
+      'warn',
+      'as-needed',
+      { requireReturnForObjectLiteral: true },
+    ],
+    'no-shadow': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
       {
-        js: "never",
-        jsx: "never",
-        ts: "never",
-        tsx: "never",
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
       },
     ],
-    "import/no-extraneous-dependencies": [
-      "error",
+    'import/no-extraneous-dependencies': [
+      'error',
       {
-        devDependencies: ["**/*.spec.ts", "**/*.spec.tsx"],
+        devDependencies: ['**/*.spec.ts', '**/*.spec.tsx'],
       },
     ],
-    "import/prefer-default-export": "off",
-    "no-underscore-dangle": [
-      "error",
+    'import/prefer-default-export': 'off',
+    'no-underscore-dangle': [
+      'error',
       {
         allowAfterThis: true,
       },
     ],
-    "class-methods-use-this": "off",
+    'class-methods-use-this': 'off',
   },
-  overrides: [
-    {
-      files: ["**/*.spec.ts"],
-      env: {
-        jest: true,
-      },
-      plugins: ["jest"],
-      rules: {
-        "jest/no-disabled-tests": "warn",
-        "jest/no-focused-tests": "error",
-        "jest/no-identical-title": "error",
-        "jest/prefer-to-have-length": "warn",
-        "jest/valid-expect": "error",
-      },
-    },
-  ],
 };
